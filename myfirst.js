97bbfdab8ad220ef2082ce011eae0ac96d722e47
Peter McGeeney <Peter.McGeeney@tonikenergy.com>
2018-03-24 16:29:14 +0000

    with date stuff

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,9 @@
 var http = require('http');
+var dateStuff = require('./myfirstmodule.js');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    var dateNow = dateStuff.myDateTime();
+    res.end(dateNow);
+    // res.end('Hello World!');
 }).listen(8080);
