ba98d0cffa97d529f4d12b2dcab5fa37d1d873f4
Peter McGeeney <Peter.McGeeney@tonikenergy.com>
2018-03-24 18:08:06 +0000

    loads

--- myfirst.js
+++ myfirst.js
@@ -1,9 +1,12 @@
 var http = require('http');
-var dateStuff = require('./myfirstmodule.js');
+var dt = require('./myfirstmodule.js');
+var url = require('url');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    var dateNow = dateStuff.myDateTime();
-    res.end(dateNow);
-    // res.end('Hello World!');
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    //res.write("The date and time are currently: " + dt.myDateTime());
+    //res.write(req.url);
+    res.end(txt);
 }).listen(8080);
